c92f548cfa13dc02103aa9e9aeb4941ec22638a0
aralhibshi <118065642+aralhibshi@users.noreply.github.com>
2023-10-22 12:53:47 +0300

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native', 'prettier'],
+  plugins: ['react', 'prettier', 'standard'],
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    'max-len': ['warn', {code: 120}],
+  },
+};
